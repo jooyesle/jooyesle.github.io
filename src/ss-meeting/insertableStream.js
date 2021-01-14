232b059d6ce8291c3b6736fc308e23711530b4eb
yeseuljoo <dptmfe93@naver.com>
2021-01-14 17:00:10 +0900

    Create insertableStream.js

---
+++ src/ss-meeting/insertableStream.js
@@ -0,0 +1,15 @@
+function handleEncryption(checkbox){
+  if(checkbox.checked == true){
+    console.log("Encryption enabled");
+  } else{
+    console.log("Encryption disabled");
+  }
+}
+
+function handleFaceDetection(checkbox){
+if(checkbox.checked == true){
+    console.log("Face Detection enabled");
+  } else{
+    console.log("Face Detection disabled");
+  }
+}
