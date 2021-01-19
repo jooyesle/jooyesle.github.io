81b3e33af3dfd4283c68ac9d1881879afebb6ba6
yeseuljoo <dptmfe93@naver.com>
2021-01-19 11:23:38 +0900

    Update main.js

--- src/smartscreen/js/main.js
+++ src/smartscreen/js/main.js
@@ -11,7 +11,7 @@ const localVideo = document.getElementById("local_video");
 const remoteVideo = document.getElementById("remote_video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('face-api/models')//,
+  faceapi.nets.tinyFaceDetector.loadFromUri('/src/smart/screen/js/face-api/models')//,
   //faceapi.nets.faceLandmark68Net.loadFromUri('face-api/models'),
   //faceapi.nets.faceRecognitionNet.loadFromUri('face-api/models'),
   //faceapi.nets.faceExpressionNet.loadFromUri('face-api/models')
