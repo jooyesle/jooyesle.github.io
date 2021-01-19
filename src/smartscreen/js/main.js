cb1f2c740d6dc5a6509cd490780272cf769e3ee9
yeseul.joo <dptmfe93@naver.com>
2021-01-19 16:42:40 +0900

    face detection in local video

--- src/smartscreen/js/main.js
+++ src/smartscreen/js/main.js
@@ -11,16 +11,24 @@ const localVideo = document.getElementById("local_video");
 const remoteVideo = document.getElementById("remote_video");
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/src/smartscreen/js/face-api/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/src/smartscreen/js/face-api/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/src/smartscreen/js/face-api/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/src/smartscreen/js/face-api/models')
-]).then(startVideo)
+  faceapi.nets.tinyFaceDetector.loadFromUri(
+    "/src/smartscreen/js/face-api/models"
+  ),
+  faceapi.nets.faceLandmark68Net.loadFromUri(
+    "/src/smartscreen/js/face-api/models"
+  ),
+  faceapi.nets.faceRecognitionNet.loadFromUri(
+    "/src/smartscreen/js/face-api/models"
+  ),
+  faceapi.nets.faceExpressionNet.loadFromUri(
+    "/src/smartscreen/js/face-api/models"
+  ),
+]).then(startVideo);
 
 localVideo.addEventListener("playing", () => {
   const canvas = faceapi.createCanvasFromMedia(localVideo);
   document.body.append(canvas);
-  const displaySize = { width: localVideo.width, height: localVideo.height}
+  const displaySize = { width: localVideo.width, height: localVideo.height };
   setInterval(async () => {
     const detections = await faceapi
       .detectAllFaces(localVideo, new faceapi.TinyFaceDetectorOptions())
@@ -28,7 +36,7 @@ localVideo.addEventListener("playing", () => {
       .withFaceExpressions();
     //console.log(detections);
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+    canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
     faceapi.draw.drawDetections(canvas, resizedDetections);
   }, 100);
 });
@@ -73,7 +81,7 @@ function startVideo() {
   printLog("supportsInsertableStreams=" + supportsInsertableStreams);
 
   console.log("startVideo()");
-  const constraints = { video: { width: 640, height: 480 }, audio: true };
+  const constraints = { video: { width: 320, height: 240 }, audio: true };
   console.log("getUserMedia constraints:", constraints);
 
   navigator.mediaDevices
